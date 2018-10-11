d3020aa907f9e99a7b9996e95081a0ae7ab80395
JoseEric1 <animesfallen@gmail.com>
2018-10-11 18:06:26 -0300

    Modificação 2

--- src/main/java/tabbedContainerTC/TabbedContainerTCApplication.java
+++ src/main/java/tabbedContainerTC/TabbedContainerTCApplication.java
@@ -5,6 +5,6 @@ import totalcross.TotalCrossApplication;
 public class TabbedContainerTCApplication {
 	
 		public static void main(String[] args) {
-			TotalCrossApplication.run(TabbedContainerTC.class, "/r", "5443535418482B72E881B6EE");
+			TotalCrossApplication.run(TabbedContainerTC.class, "/r", "put your key here");
 		}
 }
